1e9829772ac7eddd37543bfeef7ba0fa66fe6e09
Bazzikk <bazanmikha@googlemail.com>
2024-10-10 21:49:47 +0300

    Compleate APP

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import client.ClientGUI;
+import server.ServerWindow;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        ServerWindow serverWindow = new ServerWindow();
+        new ClientGUI(serverWindow);
+        new ClientGUI(serverWindow);
+    }
+}
